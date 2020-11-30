1ad66f3dcc97871e36a07bd71c34094d8fc0581b
unknown <amitesh007ranjan@gmail.com>
2020-12-01 00:51:16 +1100

    added project structur and prop types

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Auxilliary from '../../hoc/Auxilliary';
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Auxilliary>
         <div>Navbar, DropBox</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Auxilliary>
