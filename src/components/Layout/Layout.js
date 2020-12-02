3395dd4b607e176e8c0b551b03f9c5f857fa7a15
unknown <amitesh007ranjan@gmail.com>
2020-12-02 19:04:03 +1100

    made sidedrawer functional and attached burger icon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Auxilliary from '../../hoc/Auxilliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Auxilliary>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Auxilliary>
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-);
+    SideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {
+                showSideDrawer: !prevState.showSideDrawer
+            }
+        })
+    }
 
-export default layout;
+    closeSideDrawerHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        })
+    }
+
+
+    render () {
+        return (
+            <Auxilliary>
+                <Toolbar drawToggleClicked={this.SideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.closeSideDrawerHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxilliary>
+        );
+    }
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,11 +2,18 @@ import React from 'react';
 import classes from './SideDrawer.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Auxilliary from '../../../hoc/Auxilliary';
 const sideDrawer = (props) => {
 
+    let classLists = [classes.SideDrawer, classes.Close]
+    if (props.open) {
+        classLists = [classes.SideDrawer, classes.Open]
+    }
     return (
-        <div className={classes.SideDrawer}>
+        <Auxilliary>
+        <Backdrop show={props.open} clicked={props.closed}/>
+        <div className={classLists.join(' ')}>
         <div className={classes.Logo}>
             <Logo />
         </div>
@@ -15,6 +22,8 @@ const sideDrawer = (props) => {
             </nav>
 
         </div>
+        </Auxilliary>
+
     );
 }
 
