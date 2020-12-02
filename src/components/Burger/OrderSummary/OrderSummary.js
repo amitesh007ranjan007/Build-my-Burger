fbce68a6a8c5cc205fe56a973494de118ecb439e
unknown <amitesh007ranjan@gmail.com>
2020-12-02 20:03:51 +1100

    modified orderSummary for media query

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Auxilliary from '../../../hoc/Auxilliary';
 import Button from '../../UI/Button/Button';
+import classes from './OrderSummary.css';
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
      .map(igKey => {
@@ -17,8 +18,10 @@ const orderSummary = (props) => {
             </ul>
     <p><strong>Total price : {props.price}</strong></p>
             <p>Please continue to checkout..</p>
+            <div className={classes.ButtonAlign}>
             <Button buttonType="Danger" clicked={props.cancelPurchase}>CANCEL</Button>
             <Button buttonType="Success" clicked={props.continuePurchase}>CONTINUE</Button>
+            </div>
         </Auxilliary>
     );
 }
