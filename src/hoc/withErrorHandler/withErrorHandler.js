0fbcd62f6a528dddbc6c33b0ca53beca9670ae38
unknown <amitesh007ranjan@gmail.com>
2020-12-12 09:07:10 +1100

    added axios err handler usinh HOC

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import Auxilliary from '../Auxilliary';
+import  Modal from '../../components/UI/Modal/Modal';
+
+const withErrorHandler = (WrappedComponent, axios) => {
+    return class extends Component {
+        state = {
+            error: null
+        }
+        componentDidMount() {
+            axios.interceptors.request.use(req => {
+                this.setState({error:null});
+                return req;
+            });
+            axios.interceptors.response.use(null, error => {
+                this.setState({error:error});
+            });
+        }
+
+        modalClosedHandler = () => {
+            this.setState({error: null});
+        }
+        render() {
+            return (
+                <Auxilliary>
+                    <Modal show={this.state.error} modalClosed={this.modalClosedHandler}>
+                        {this.state.error ? this.state.error.message : null }
+                    </Modal>
+                    <WrappedComponent {...this.props}/>
+                </Auxilliary>
+                
+            )
+
+        }
+    }
+
+}
+
+export default withErrorHandler;
