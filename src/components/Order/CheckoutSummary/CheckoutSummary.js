34c651942415f8d010a1e5618f26793de608ed4d
unknown <amitesh007ranjan@gmail.com>
2020-12-14 17:05:19 +1100

    Set up the routes

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.css';
+
+const checkoutSummary = (props) => {
+    return (
+        <div className={classes.CheckoutSumary}>
+            <h1>We hope you have a great time having this burger!</h1>
+            <div style={{width: '100%', margin:'auto'}}>
+                <Burger ingredients={props.ingredients}/>
+            </ div>
+            <div style={{textAlign: 'Center'}}>
+            <Button 
+                buttonType='Danger' 
+                clicked>CANCEL</Button>
+            <Button 
+                buttonType='Success' 
+                clicked>CONTINUE</Button>
+            </div>
+        </div>
+    )
+
+}
+
+export default checkoutSummary;
